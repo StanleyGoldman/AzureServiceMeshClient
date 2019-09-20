fa1e5b7e0bf249e61fc5176d939623bdf116389d
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-09-20 11:06:58 -0400

    Removing subcommand

--- src/Client.App/Extensions/CommandLineApplicationExtensions.cs
+++ src/Client.App/Extensions/CommandLineApplicationExtensions.cs
@@ -6,14 +6,5 @@ namespace Client.App.Extensions
 {
     public static class CommandLineApplicationExtensions
     {
-        public static void HandleValidationError(this CommandLineApplication commandLineApplication)
-        {
-            commandLineApplication.OnValidationError(result =>
-            {
-                ColorConsole.WithRedText.WriteLine(result.ToString());
-                Console.WriteLine();
-                commandLineApplication.ShowHelp();
-            });
-        }
     }
 }
--- src/Client.App/Program.cs
+++ src/Client.App/Program.cs
@@ -5,6 +5,7 @@ using AutofacSerilogIntegration;
 using Client.App.Extensions;
 using Client.App.FirstScenario;
 using Client.App.Serilog;
+using FluentColorConsole;
 using McMaster.Extensions.CommandLineUtils;
 using Serilog;
 
@@ -55,149 +56,146 @@ namespace Client.App
             };
 
             app.HelpOption(true);
-            app.Command("scenario1", configCmd =>
+
+            const string subscriptionIdEnvVar = "DDOAAMC_SubscriptionId";
+            const string tenantIdEnvVar = "DDOAAMC_TenantId";
+            const string clientSecretEnvVar = "DDOAAMC_ClientSecret";
+            const string imageRegistryPasswordEnvVar = "DDOAAMC_ImageRegistryPassword";
+            const string azurePipelinesTokenEnvVar = "DDOAAMC_AzurePipelinesToken";
+
+            var name = app
+                .Option("-n|--name", "Mesh Name",
+                    CommandOptionType.SingleValue)
+                .IsRequired();
+
+            var imageName = app
+                .Option("-i|--imageName", "Image Name",
+                    CommandOptionType.SingleValue)
+                .IsRequired();
+
+            var resourceGroup = app
+                .Option("-rg|--resourceGroup", "Resource Group",
+                    CommandOptionType.SingleValue)
+                .IsRequired();
+
+            var clientId = app
+                .Option("-ci|--clientId", "Client ID", 
+                    CommandOptionType.SingleValue)
+                .IsRequired();
+
+            var clientSecret = app
+                .Option("-cs|--clientSecret", $"Client Secret (or env:'{clientSecretEnvVar}')",
+                    CommandOptionType.SingleValue)
+                .IsRequired();
+
+            var tenantId = app
+                .Option("-t|--tenantId", $"Tenant ID (or env:'{tenantIdEnvVar}')",
+                    CommandOptionType.SingleValue)
+                .IsRequired();
+
+            var subscriptionId = app
+                .Option("-s|--subscriptionId", $"Subscription ID (or env:'{subscriptionIdEnvVar}')",
+                    CommandOptionType.SingleValue)
+                .IsRequired();
+
+            var imageRegistryServer = app
+                .Option("-irs|--imageRegistryServer", "Docker Image Registry Server",
+                    CommandOptionType.SingleValue)
+                .IsRequired();
+
+            var imageRegistryUsername = app
+                .Option("-iru|--imageRegistryUsername", "Docker Image Registry Username",
+                    CommandOptionType.SingleValue)
+                .IsRequired();
+
+            var imageRegistryPassword = app
+                .Option("-irp|--imageRegistryPassword",
+                    "Docker Image Registry Password (or env:'" + imageRegistryPasswordEnvVar + "')",
+                    CommandOptionType.SingleValue)
+                .IsRequired();
+
+            var azpUrl = app
+                .Option("-azpu|--azurePipelinesUrl", "Azure Pipelines Url",
+                    CommandOptionType.SingleValue)
+                .IsRequired();
+
+            var azpToken = app
+                .Option("-azpt|--azurePipelinesToken",
+                    "Azure Pipelines Token (or env:'" + imageRegistryPasswordEnvVar + "')",
+                    CommandOptionType.SingleValue)
+                .IsRequired();
+
+            app.OnParsingComplete(context =>
             {
-                const string subscriptionIdEnvVar = "DDOAAMC_SubscriptionId";
-                const string tenantIdEnvVar = "DDOAAMC_TenantId";
-                const string clientSecretEnvVar = "DDOAAMC_ClientSecret";
-                const string imageRegistryPasswordEnvVar = "DDOAAMC_ImageRegistryPassword";
-                const string azurePipelinesTokenEnvVar = "DDOAAMC_AzurePipelinesToken";
-
-                var name = configCmd
-                    .Option("-n|--name", "Mesh Name",
-                        CommandOptionType.SingleValue)
-                    .IsRequired();
-
-                var imageName = configCmd
-                    .Option("-i|--imageName", "Image Name",
-                        CommandOptionType.SingleValue)
-                    .IsRequired();
-
-                var resourceGroup = configCmd
-                    .Option("-rg|--resourceGroup", "Resource Group",
-                        CommandOptionType.SingleValue)
-                    .IsRequired();
-
-                var clientId = configCmd
-                    .Option("-ci|--clientId", "Client ID", 
-                        CommandOptionType.SingleValue)
-                    .IsRequired();
-
-                var clientSecret = configCmd
-                    .Option("-cs|--clientSecret", $"Client Secret (or env:'{clientSecretEnvVar}')",
-                        CommandOptionType.SingleValue)
-                    .IsRequired();
-
-                var tenantId = configCmd
-                    .Option("-t|--tenantId", $"Tenant ID (or env:'{tenantIdEnvVar}')",
-                        CommandOptionType.SingleValue)
-                    .IsRequired();
-
-                var subscriptionId = configCmd
-                    .Option("-s|--subscriptionId", $"Subscription ID (or env:'{subscriptionIdEnvVar}')",
-                        CommandOptionType.SingleValue)
-                    .IsRequired();
-
-                var imageRegistryServer = configCmd
-                    .Option("-irs|--imageRegistryServer", "Docker Image Registry Server",
-                        CommandOptionType.SingleValue)
-                    .IsRequired();
-
-                var imageRegistryUsername = configCmd
-                    .Option("-iru|--imageRegistryUsername", "Docker Image Registry Username",
-                        CommandOptionType.SingleValue)
-                    .IsRequired();
-
-                var imageRegistryPassword = configCmd
-                    .Option("-irp|--imageRegistryPassword",
-                        "Docker Image Registry Password (or env:'" + imageRegistryPasswordEnvVar + "')",
-                        CommandOptionType.SingleValue)
-                    .IsRequired();
-
-                var azpUrl = configCmd
-                    .Option("-azpu|--azurePipelinesUrl", "Azure Pipelines Url",
-                        CommandOptionType.SingleValue)
-                    .IsRequired();
-
-                var azpToken = configCmd
-                    .Option("-azpt|--azurePipelinesToken",
-                        "Azure Pipelines Token (or env:'" + imageRegistryPasswordEnvVar + "')",
-                        CommandOptionType.SingleValue)
-                    .IsRequired();
-
-                configCmd.OnParsingComplete(context =>
+                if (!clientSecret.Values.Any())
                 {
-                    if (!clientSecret.Values.Any())
-                    {
-                        var value = Environment.GetEnvironmentVariable(clientSecretEnvVar);
-                        if (!string.IsNullOrWhiteSpace(value)) clientSecret.Values.Add(value);
-                    }
+                    var value = Environment.GetEnvironmentVariable(clientSecretEnvVar);
+                    if (!string.IsNullOrWhiteSpace(value)) clientSecret.Values.Add(value);
+                }
 
-                    if (!imageRegistryPassword.Values.Any())
-                    {
-                        var value = Environment.GetEnvironmentVariable(imageRegistryPasswordEnvVar);
-                        if (!string.IsNullOrWhiteSpace(value)) imageRegistryPassword.Values.Add(value);
-                    }
-
-                    if (!azpToken.Values.Any())
-                    {
-                        var value = Environment.GetEnvironmentVariable(azurePipelinesTokenEnvVar);
-                        if (!string.IsNullOrWhiteSpace(value)) azpToken.Values.Add(value);
-                    }
-
-                    if (!subscriptionId.Values.Any())
-                    {
-                        var value = Environment.GetEnvironmentVariable(subscriptionIdEnvVar);
-                        if (!string.IsNullOrWhiteSpace(value)) subscriptionId.Values.Add(value);
-                    }
-
-                    if (!tenantId.Values.Any())
-                    {
-                        var value = Environment.GetEnvironmentVariable(tenantIdEnvVar);
-                        if (!string.IsNullOrWhiteSpace(value)) tenantId.Values.Add(value);
-                    }
-                });
-
-                configCmd.HandleValidationError();
-
-                configCmd.OnExecute(() =>
+                if (!imageRegistryPassword.Values.Any())
                 {
-                    var containerBuilder = GetContainerBuilder();
+                    var value = Environment.GetEnvironmentVariable(imageRegistryPasswordEnvVar);
+                    if (!string.IsNullOrWhiteSpace(value)) imageRegistryPassword.Values.Add(value);
+                }
 
-                    containerBuilder.RegisterType<InputManager>()
-                        .AsSelf();
+                if (!azpToken.Values.Any())
+                {
+                    var value = Environment.GetEnvironmentVariable(azurePipelinesTokenEnvVar);
+                    if (!string.IsNullOrWhiteSpace(value)) azpToken.Values.Add(value);
+                }
 
-                    containerBuilder.RegisterType<MeshController>()
-                        .AsSelf();
+                if (!subscriptionId.Values.Any())
+                {
+                    var value = Environment.GetEnvironmentVariable(subscriptionIdEnvVar);
+                    if (!string.IsNullOrWhiteSpace(value)) subscriptionId.Values.Add(value);
+                }
 
-                    var container = containerBuilder.Build();
+                if (!tenantId.Values.Any())
+                {
+                    var value = Environment.GetEnvironmentVariable(tenantIdEnvVar);
+                    if (!string.IsNullOrWhiteSpace(value)) tenantId.Values.Add(value);
+                }
+            });
 
-                    var firstScenarioManager = container.Resolve<InputManager>();
-
-                    var arguments = new Arguments
-                    {
-                        Name = name.Value(),
-                        ImageName = imageName.Value(),
-                        ClientId = clientId.Value(),
-                        ClientSecret = clientSecret.Value(),
-                        TenantId = tenantId.Value(),
-                        SubscriptionId = subscriptionId.Value(),
-                        ImageRegistryServer = imageRegistryServer.Value(),
-                        ImageRegistryUsername = imageRegistryUsername.Value(),
-                        ImageRegistryPassword = imageRegistryPassword.Value(),
-                        ResourceGroup = resourceGroup.Value(),
-                        AzurePipelinesUrl = azpUrl.Value(),
-                        AzurePipelinesToken = azpToken.Value()
-                    };
-
-                    return firstScenarioManager.Start(arguments);
-                });
+            app.OnValidationError(result =>
+            {
+                ColorConsole.WithRedText.WriteLine(result.ToString());
+                Console.WriteLine();
+                app.ShowHelp();
             });
 
             app.OnExecute(() =>
             {
-                app.ShowHelp();
-                return 1;
+                var containerBuilder = GetContainerBuilder();
+
+                containerBuilder.RegisterType<InputManager>()
+                    .AsSelf();
+
+                containerBuilder.RegisterType<MeshController>()
+                    .AsSelf();
+
+                var container = containerBuilder.Build();
+
+                var firstScenarioManager = container.Resolve<InputManager>();
+
+                var arguments = new Arguments
+                {
+                    Name = name.Value(),
+                    ImageName = imageName.Value(),
+                    ClientId = clientId.Value(),
+                    ClientSecret = clientSecret.Value(),
+                    TenantId = tenantId.Value(),
+                    SubscriptionId = subscriptionId.Value(),
+                    ImageRegistryServer = imageRegistryServer.Value(),
+                    ImageRegistryUsername = imageRegistryUsername.Value(),
+                    ImageRegistryPassword = imageRegistryPassword.Value(),
+                    ResourceGroup = resourceGroup.Value(),
+                    AzurePipelinesUrl = azpUrl.Value(),
+                    AzurePipelinesToken = azpToken.Value()
+                };
+
+                return firstScenarioManager.Start(arguments);
             });
 
             return app.Execute(args);
